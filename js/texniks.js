2633afa7999f477bcca5dd4c81c9dd6b63a8c985
Mher Janikyan <mher.janikyan01@gmail.com>
2021-09-26 22:46:27 +0400

    messig

--- js/texniks.js
+++ js/texniks.js
@@ -1,3 +1,37 @@
+let order_add = document.getElementById('order_add');
+let main = document.getElementById('main');
+let checkout_secton = document.getElementById('checkout_secton');
+let your_datasection = document.getElementById('your_datasection');
+let next_option = document.getElementById('next_option');
+let next_option2 = document.getElementById('next_option2');
+
+
+    order_add.addEventListener("click",()=>{
+        main.style.display="none";
+        checkout_secton.style.display="flex";
+    }); 
+
+
+    next_option.addEventListener("click",()=>{
+        your_datasection.style.display="none";
+        your_datasection2.style.display="flex";
+    });
+
+    next_option2.addEventListener("click",()=>{
+        your_datasection2.style.display="none";
+        your_datasection3.style.display="flex";
+    });
+
+    next_option3.addEventListener("click",()=>{
+        checkout_secton.style.display="none";
+        congratulations.style.display="flex";
+    });
+
+    congratulations.addEventListener("click",()=>{
+        congratulations.style.display="none";
+        arrived.style.display="flex"
+    });
+
 function toggle (){
     let nav = document.getElementById("nav");
     let openmenu = document.getElementById("toggle_menu");
@@ -37,3 +71,4 @@ function toggle (){
     });
 }
 toggle()
+

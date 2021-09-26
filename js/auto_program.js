2e1a776a5301ac087bae8cf9ab40794f044b682b
Mher15 <68960611+Mher15@users.noreply.github.com>
2021-09-26 14:38:00 +0400

    Add files via upload

--- js/auto_program.js
+++ js/auto_program.js
@@ -32,6 +32,45 @@ let has_been = document.getElementById('has_been');
 let sum = 0 ;
 
 
+function toggle (){
+    let nav = document.getElementById("nav");
+    let openmenu = document.getElementById("toggle_menu");
+
+    let cloase_menu = document.getElementById("cloase_menu")
+
+
+
+    openmenu.addEventListener("click",()=>{
+        let id = null;
+        let pos = -380;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+        function frame() {
+            if (pos == 0) {
+                clearInterval(id);
+            } else {
+                pos+=4;
+                nav.style.left = pos + "px";
+            }
+        }
+    });
+
+    cloase_menu.addEventListener("click",()=>{
+        let id = null;
+        let pos = 0;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+        function frame() {
+            if (pos == -380) {
+                clearInterval(id);
+            } else {
+                pos-=4;
+                nav.style.left = pos + "px";
+            }
+        }
+    });
+}
+toggle()
 
 
 start.addEventListener("click",()=>{

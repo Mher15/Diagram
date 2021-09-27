c200ea0b19192d6cb7405749446f3eb0cfc635bc
Mher Janikyan <mher.janikyan01@gmail.com>
2021-09-28 01:04:14 +0400

    end

---
+++ js/nesw.js
@@ -0,0 +1,53 @@
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+  function toggle (){
+    let nav = document.getElementById("nav");
+    let openmenu = document.getElementById("toggle_menu");
+    
+    let cloase_menu = document.getElementById("cloase_menu")
+
+  
+    
+    openmenu.addEventListener("click",()=>{
+        let id = null;
+        let pos = -380;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+        function frame() {
+          if (pos == 0) {
+            clearInterval(id);
+          } else {
+            pos+=4; 
+            nav.style.left = pos + "px"; 
+          }
+        }
+    });
+
+    cloase_menu.addEventListener("click",()=>{
+        let id = null;
+        let pos = 0;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+        function frame() {
+          if (pos == -380) {
+            clearInterval(id);
+          } else {
+            pos-=4; 
+            nav.style.left = pos + "px"; 
+          }
+        }
+    });
+}
+toggle()
